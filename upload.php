a143218e9c801095ec22a5ae3818f5a9fcb0b61b
张桂杨 <2239991713@qq.com>
2016-07-27 16:39:58 +0800

    commit

--- upload.php
+++ upload.php
@@ -16,10 +16,10 @@ $dbInfo = $obj["dbInfo"];
      die("打开失败: " . $conn->connect_error);
  }
 
-
-
 foreach ($dataArray as $key => $value) {
-  $sql = "INSERT INTO MyGuests (my_id,my_title,my_userName,my_pwd,my_loginURL) VALUES ('".$value["loginId"]."' ,'".$value["title"]."','".$value["userName"]."','".$value["password"]."','".$value["loginURL"]."');";
+
+
+  $sql = "INSERT INTO MyGuests (my_title,my_userName,my_pwd,my_loginURL) VALUES ('".$value["title"]."','".$value["userName"]."','".$value["password"]."','".$value["loginURL"]."') ON DUPLICATE KEY UPDATE my_id = ".$value["loginId"].";";
   echo $sql;
   $result = $conn->query($sql);
   echo $result;
