83506d22e2e9f4375ad31b62e02652895cdd81c8
张桂杨 <2239991713@qq.com>
2016-07-27 11:44:02 +0800

    no message

--- upload.php
+++ upload.php
@@ -1,19 +1,22 @@
 <?php
 header('content-type:text/json');
- $servername = "localhost";
- $username = "u849025525_mydb";
- $password = "qq1314521";
- $dbname = "u849025525_mydb";
+$postData =  file_get_contents("php://input");
+$obj = json_decode($postData,true);
+$dataArray =  $obj["data"];
+$dbInfo = $obj["dbInfo"];
+
+ $servername = $dbInfo["servername"];
+ $username = $dbInfo["username"];
+ $password = $dbInfo["password"];
+ $dbname = $dbInfo["dbname"];
 
  $conn = new mysqli($servername, $username, $password, $dbname);
 
  if ($conn->connect_error) {
-     die("&#36830;&#25509;&#22833;&#36133;: " . $conn->connect_error);
+     die("打开失败: " . $conn->connect_error);
  }
 
-$postData =  file_get_contents("php://input");
-$obj = json_decode($postData,true);
-$dataArray =  $obj["data"];
+
 
 foreach ($dataArray as $key => $value) {
   $sql = "INSERT INTO MyGuests (my_id,my_title,my_userName,my_pwd,my_loginURL) VALUES ('".$value["loginId"]."' ,'".$value["title"]."','".$value["userName"]."','".$value["password"]."','".$value["loginURL"]."');";
