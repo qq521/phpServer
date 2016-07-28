b433f9023dab2c842e1bd30875226b64a9741ea6
张桂杨 <2239991713@qq.com>
2016-07-28 09:54:47 +0800

    删除

---
+++ delete.php
@@ -0,0 +1,26 @@
+<?php
+header('content-type:text/json');
+$postData =  file_get_contents("php://input");
+$obj = json_decode($postData,true);
+$dbInfo = $obj["dbInfo"];
+$dataArray =  $obj["data"];
+
+$servername = $dbInfo["servername"];
+$username = $dbInfo["username"];
+$password = $dbInfo["password"];
+$dbname = $dbInfo["dbname"];
+
+ $conn = new mysqli($servername, $username, $password, $dbname);
+
+ if ($conn->connect_error) {
+     die("打开失败: " . $conn->connect_error);
+ }
+
+foreach ($dataArray as $key => $value) {
+  $sql = "DELETE FROM MyGuests WHERE my_id = " .$value;
+  $result = mysqli_query($sql);
+  echo json_encode($result);
+}
+
+ $conn->close();
+ ?>
