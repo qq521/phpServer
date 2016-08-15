58d5aef7be671d463de3f8a8b034790a337b0ee4
Ive <2239991713@qq.com>
2016-08-15 17:44:46 +0800

    测试参数

--- getMessage.php
+++ getMessage.php
@@ -11,26 +11,25 @@ $dbInfo = $obj["dbInfo"];
 
  $conn = new mysqli($servername, $username, $password, $dbname);
  echo json_encode($dbInfo);
- if ($conn->connect_error) {
-
-     die("打开失败: " . $conn->connect_error);
- }
-
-  $sql = "SELECT id,content FROM MessageList ;";
-  $result = $conn->query($sql);
-  if ($result == false) {
-    $response = array ("succeed"=>0);
-    echo json_encode($response);
-    return;
-  }
-  $resultArr = array();
-  while ($row = mysqli_fetch_assoc($result))
-  {
-    $array = is_object($row) ? get_object_vars($obj) : $row;
-    array_push($resultArr,$array);
-  }
-  $response = array ("succeed" => 1,"infoList"=>$resultArr);
-  echo json_encode($response);
-
- $conn->close();
+ // if ($conn->connect_error) {
+ //     die("打开失败: " . $conn->connect_error);
+ // }
+ //
+ //  $sql = "SELECT id,content FROM MessageList ;";
+ //  $result = $conn->query($sql);
+ //  if ($result == false) {
+ //    $response = array ("succeed"=>0);
+ //    echo json_encode($response);
+ //    return;
+ //  }
+ //  $resultArr = array();
+ //  while ($row = mysqli_fetch_assoc($result))
+ //  {
+ //    $array = is_object($row) ? get_object_vars($obj) : $row;
+ //    array_push($resultArr,$array);
+ //  }
+ //  $response = array ("succeed" => 1,"infoList"=>$resultArr);
+ //  echo json_encode($response);
+ //
+ // $conn->close();
  ?>
